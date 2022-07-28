89a7a388a68a680a56905063baf72cde1d1c0e34
zidnaarif <zidna.arif@gmail.com>
2022-07-28 16:43:51 +0700

    add carousel componen vue

--- src/components/CardDesign.vue
+++ src/components/CardDesign.vue
@@ -1,15 +1,94 @@
 <template>
-    <div class="card border-0 shadow">
-        <img :src="`/${image}`" alt="" class="card-img-top">
-        <div class="card-body">
-            <h4>{{title}}</h4>
-            <span>{{desc}}</span>
-        </div>
-    </div>
+  <swiper
+    :effect="'coverflow'"
+    :grabCursor="true"
+    :centeredSlides="true"
+    :slidesPerView="'auto'"
+    :coverflowEffect="{
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    }"
+    :pagination="true"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide>
+        <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                <h2>test</h2>
+    </swiper-slide>
+    <swiper-slide>
+      <img src="https://swiperjs.com/demos/images/nature-2.jpg" /> 
+      </swiper-slide>
+      <swiper-slide
+      ><img
+        src="https://swiperjs.com/demos/images/nature-3.jpg" /></swiper-slide
+    ><swiper-slide
+      ><img
+        src="https://swiperjs.com/demos/images/nature-4.jpg" /></swiper-slide
+    ><swiper-slide
+      ><img
+        src="https://swiperjs.com/demos/images/nature-5.jpg" /></swiper-slide
+    ><swiper-slide
+      ><img
+        src="https://swiperjs.com/demos/images/nature-6.jpg" /></swiper-slide
+    ><swiper-slide
+      ><img
+        src="https://swiperjs.com/demos/images/nature-7.jpg" /></swiper-slide
+    ><swiper-slide
+      ><img
+        src="https://swiperjs.com/demos/images/nature-8.jpg" /></swiper-slide
+    ><swiper-slide
+      ><img src="https://swiperjs.com/demos/images/nature-9.jpg"
+    /></swiper-slide>
+  </swiper>
 </template>
 
 <script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
+
 export default {
-        props: ['title', 'image', 'desc']
-    }
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [EffectCoverflow, Pagination],
+    };
+  },
+};
 </script>
+
+<!-- Demo styles -->
+    <style>
+        .swiper {
+        width: 100%;
+        padding-top: 50px;
+        padding-bottom: 50px;
+        }
+
+        .swiper-slide {
+        background-position: center;
+        background-size: cover;
+        width: 300px;
+        height: 300px;
+        }
+
+        .swiper-slide img {
+        display: block;
+        max-width: 100%;
+        }
+    </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,65 +30,8 @@
 
   <SearchVue/>
 
-  <section id="ourdesign" class="overflow-hidden">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-9">
-          <h2>Our Design</h2>
-        </div>
-        <div class="col-md-3 my-auto">
-          <button class="button-secondary rounded-pill">More ></button>
-        </div>
-      </div>
-    </div>
-
-    <div class="row mt-5 container mx-5">
-      <div class="container-fluid">
-        <div class="col-12 subtittle text-center">
-          <span>We design & deliver beautiful hotels, luxury homes and innovative workplace interiors. 
-            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem.</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="container position-relative">
-      <div class="row mt-5">
-        <div class="owl-carousel owl-theme">
-          <div class="item mb-4">
-            <CardDesign :image="'assets/img/brown-living-room.jpg'" title="Living Room" desc="Model A-11"/>
-          </div>
-
-          <div class="item mb-4">
-            <CardDesign :image="'assets/img/green-living.jpg'" title="Living Room" desc="Model A-13"/>
-          </div>
-
-          <div class="item mb-4">
-            <CardDesign :image="'assets/img/kitchen-studio.jpg'" title="Kitchen" desc="Model B-11"/>
-          </div>
-
-          <div class="item mb-4">
-            <CardDesign :image="'assets/img/luxury-bedroom.jpg'" title="Bed Room" desc="Model C-22"/>
-          </div>
-
-          <div class="item mb-4">
-            <CardDesign :image="'assets/img/brown-living-room.jpg'" title="Living Room" desc="Model A-18"/>
-          </div>
-
-          <div class="item mb-4">
-            <CardDesign :image="'assets/img/green-living.jpg'" title="Living Room" desc="Model A-12"/>
-          </div>
-
-          <div class="item mb-4">
-            <CardDesign :image="'assets/img/kitchen-studio.jpg'" title="Kitchen" desc="Model B-11"/>
-          </div>
-
-          <div class="item mb-4">
-            <CardDesign :image="'assets/img/luxury-bedroom.jpg'" title="Bed Room" desc="Model C-22"/>
-          </div>
-          
-        </div>
-      </div>
-    </div>
+  <section id="ourdesign">
+    <CardDesign/>
   </section>
 
   <ReviewVue/>
@@ -105,8 +48,8 @@ import ServiceVue from '../components/ServiceVue.vue'
 import SearchVue from '../components/SearchVue.vue'
 import FooterVue from '../components/FooterVue.vue'
 import ContactVue from '../components/ContactVue.vue'
-import CardDesign from '../components/CardDesign.vue'
 import ReviewVue from '../components/ReviewVue.vue'
+import CardDesign from '@/components/CardDesign.vue'
 
 export default {
   name: 'HomeView',
